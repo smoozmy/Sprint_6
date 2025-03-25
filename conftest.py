acd15239b75bdd9de8fa4c91338013e665f390ab
smoozmy <smoozmy@yandex.ru>
2025-03-25 12:18:10 +0300

    Задана предварительная структура проекта

---
+++ conftest.py
@@ -0,0 +1,11 @@
+from idlelib.tree import wheel_event
+
+from selenium import webdriver
+import pytest
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
